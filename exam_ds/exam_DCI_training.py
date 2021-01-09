7a80063ba82cdd659f251bb5899a42df53c15c25
ethanmac <ethanmac@mac.com>
2021-01-09 22:17:29 +0800

    1920

--- exam_ds/exam_DCI_training.py
+++ exam_ds/exam_DCI_training.py
@@ -468,8 +468,9 @@ def load_dataset():
 
 def exam_model(model):
     if not hasattr(model, "model_examed"):
-        print(model)
-        summary(model, (6, 200))
+        if not torch.cuda.is_available():
+            print(model)
+            summary(model, (6, 200))
         model.model_examed = True
 
 def get_model_from_new_training(T, epochs_num=10, save_model=False):
