8382ddec5ed2766e7abfa612e6dc1d3721e4d624
ethanmac <ethanmac@mac.com>
2021-01-11 00:03:25 +0800

    update

--- exam_ds/ex_model_ds_lstm.py
+++ exam_ds/ex_model_ds_lstm.py
@@ -35,7 +35,8 @@ class vel_regressor_lstm(torch.nn.Module):
         self.lstm = torch.nn.LSTM(self.input_size, self.lstm_size, self.num_layers, batch_first=True, dropout=dropout)
         self.linear1 = torch.nn.Linear(self.lstm_size, self.output_size * 60)
         self.linear2 = torch.nn.Linear(6 * 60, 60)
-        self.linear3 = torch.nn.Linear(60, self.output_size)
+        self.linear3 = torch.nn.Linear(60, 3)        
+        self.linear4 = torch.nn.Linear(3, self.output_size)
         #self.hidden = self.init_weights()
 
     def forward(self, input, hidden=None):
@@ -45,6 +46,7 @@ class vel_regressor_lstm(torch.nn.Module):
         output = output.view(output.size(0), -1)
         output = self.linear2(output)
         output = self.linear3(output)
+        output = self.linear4(output)        
         return output
 
 
