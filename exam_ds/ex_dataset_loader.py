69f0dac9316d90981171460b3c46a7b7717a0d52
ethanmac <ethanmac@mac.com>
2021-01-10 21:22:00 +0800

    update

--- exam_ds/ex_dataset_loader.py
+++ exam_ds/ex_dataset_loader.py
@@ -118,10 +118,10 @@ def plot_dataset(T):
     index=(np.round(np.linspace(0,len(T),1000)))
     for i in index:
         data=T[int(i)]
-        velo.append(data['gt'].cpu().numpy())
-        sp.append((data['gt'].cpu().norm()))
+        velo.append(data['gt'].cpu.numpy())
+        sp.append((data['gt'].cpu.norm()))
         t.append(data['time'])
-        
+
     plt.figure()
     plt.plot(velo)
     plt.title('Velocity Vector')
